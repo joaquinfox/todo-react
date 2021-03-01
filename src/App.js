980df0d06b40969ff2e27c5b88f5fb953f0a9266
Joaquin Fox <Joaquin.Fox@gmail.com>
2021-03-01 14:31:16 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+  const [taskList, setTaskList] = useState([]);
+  const [task, setTask] = useState('');
+  const [isError, setIsError] = useState(false);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setTaskList([...taskList, task]);
+    setTask('');
+  };
+
+  const handleNew = (e) => {
+    let newItem = { name: e.target.value, id: new Date().getTime().toString() };
+    setTask(newItem);
+  };
+
+  const handleEdit = (id, editedTask) => {
+    let editedTaskList = taskList;
+  };
+  const handleDelete = (id) => {
+    let remainingTasks = taskList.filter((task) => task.id !== id);
+    setTaskList(remainingTasks);
+    console.log(taskList);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      {isError && <p>No empty entries please.</p>}
+      <div className="container">
+        <h1>Todo</h1>
+        <form onSubmit={handleSubmit}>
+          <input type="text" onChange={handleNew} value={task.name} />
+          <button>+</button>
+        </form>
+        <section id="todo">
+          {taskList.map((i) => {
+            const { name, id } = i;
+            return (
+              <li key={id}>
+                {name}
+                <span id="button-container">
+                  <span onClick={() => handleDelete(id)}>X</span>
+                  <span>E</span>
+                </span>
+              </li>
+            );
+          })}
+        </section>
+      </div>
+    </main>
   );
 }
 
